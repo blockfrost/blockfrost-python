f62f2e961cb5965f0bd3c3b7e269e8bc6b63de91
Mathias Frøhlich <5001905+mathiasfrohlich@users.noreply.github.com>
2021-11-23 23:43:25 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (HERE / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='blockfrost-python',
-    version='0.3.2',
+    version='0.3.3',
     description='The official Python SDK for Blockfrost API v0.1.32',
     long_description=long_description,
     long_description_content_type='text/markdown',
